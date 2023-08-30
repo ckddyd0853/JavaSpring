1adafcbe5e254c80aaa18197b288b386c5bae207
신창용 <ckddyd0853@naver.com>
2023-08-30 12:29:38 +0900

    자바 부트스프링 2일차 회원서비스개발까지 끝냄

---
+++ hello-spring/src/main/java/hellp/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hellp.hellospring.repository;
+
+import hellp.hellospring.domain.Member;
+
+import java.util.Optional;
+import java.util.List;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
